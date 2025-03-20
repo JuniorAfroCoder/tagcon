8debc389290880b341cc8d84ef6a2079e80cd724
JuniorAfroCoder <murengukojesusd@gmail.com>
2025-03-20 19:04:53 +0200

    Registration accordion added

--- src/views/Registration.vue
+++ src/views/Registration.vue
@@ -1,7 +1,7 @@
 <script setup>
 import RegistrationBtn from '@/components/RegistrationBtn.vue'
 import { ref } from 'vue'
-const formHidden = ref(true)
+const formVisible = ref(true)
 </script>
 
 <template>
@@ -20,7 +20,7 @@ const formHidden = ref(true)
         <h2
           class="font-[Recharge] uppercase text-3xl font-bold text-white md:text-5xl dark:text-white"
         >
-          Ready to <span>Join Us</span> ?
+          Ready to <span class="text-[#ff5722]">Join Us</span> ?
         </h2>
 
         <p
@@ -31,112 +31,198 @@ const formHidden = ref(true)
         </p>
 
         <div class="grid grid-cols-1 gap-4 mt-8 content-center max-w-screen">
-          <RegistrationBtn message="Register as Attendee" />
-          <RegistrationBtn message="Register as Exhibitor" @click="formHidden = !formHidden" />
+          <div id="accordion-collapse" data-accordion="collapse">
+            <h2 id="accordion-collapse-heading-1">
+              <button
+                @click="formVisible = !formVisible"
+                type="button"
+                class="flex items-center justify-between w-full p-5 font-medium rtl:text-right text-gray-500 border border-b-0 border-gray-200 rounded-t-xl focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-800 dark:border-gray-700 dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-800 gap-3"
+                data-accordion-target="#accordion-collapse-body-1"
+                aria-expanded="true"
+                aria-controls="accordion-collapse-body-1"
+              >
+                <span class="text-white">Register as Exhibitor</span>
+                <svg
+                  data-accordion-icon
+                  class="w-3 h-3 rotate-180 shrink-0"
+                  aria-hidden="true"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 10 6"
+                >
+                  <path
+                    stroke="currentColor"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    stroke-width="2"
+                    d="M9 5 5 1 1 5"
+                  />
+                </svg>
+              </button>
+            </h2>
+            <div id="accordion-collapse-body-1" aria-labelledby="accordion-collapse-heading-1">
+              <div
+                :class="{ hidden: formVisible }"
+                class="p-5 border border-b-0 border-gray-200 dark:border-gray-700 dark:bg-gray-900"
+              >
+                <form class="font-[Avenir_Next_Roman]">
+                  <div class="relative z-0 mb-5 group">
+                    <input
+                      type="text"
+                      name="floating_contact_name"
+                      id="floating_contact_name"
+                      class="block py-2.5 px-0 w-full text-sm text-white bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 focus:outline-none focus:ring-0 focus:border-[#d34202] peer"
+                      placeholder=" "
+                      required
+                    />
+                    <label
+                      for="floating_contact_name"
+                      class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-2 left-2 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-[#d34202] peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
+                      >Contact name</label
+                    >
+                  </div>
+                  <div class="relative z-0 w-full mb-5 group">
+                    <input
+                      type="email"
+                      name="floating_email"
+                      id="floating_email"
+                      class="block py-2.5 px-0 w-full text-sm text-white bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 focus:outline-none focus:ring-0 focus:border-[#d34202] peer"
+                      placeholder=" "
+                      required
+                    />
+                    <label
+                      for="floating_email"
+                      class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 left-2 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto peer-focus:text-[#d34202] peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
+                      >Email address</label
+                    >
+                  </div>
 
-          <form :class="{ hidden: formHidden }" class="font-[Avenir_Next_Roman]">
-            <div class="relative z-0 mb-5 group">
-              <input
-                type="text"
-                name="floating_contact_name"
-                id="floating_contact_name"
-                class="block py-2.5 px-0 w-full text-sm text-white bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 focus:outline-none focus:ring-0 focus:border-[#d34202] peer"
-                placeholder=" "
-                required
-              />
-              <label
-                for="floating_contact_name"
-                class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-2 left-2 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-[#d34202] peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
-                >Contact name</label
-              >
+                  <div class="relative z-0 w-full mb-5 group">
+                    <input
+                      type="text"
+                      name="floating_company"
+                      id="floating_company"
+                      class="block py-2.5 px-0 w-full text-sm text-white bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 focus:outline-none focus:ring-0 focus:border-[#d34202] peer"
+                      placeholder=" "
+                      required
+                    />
+                    <label
+                      for="floating_company"
+                      class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 left-2 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-[#d34202] peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
+                      >Company (Ex. Nuyu Agency)</label
+                    >
+                  </div>
+                  <div class="relative z-0 w-full mb-5 group">
+                    <input
+                      type="tel"
+                      name="floating_phone"
+                      id="floating_phone"
+                      class="block py-2.5 px-0 w-full text-sm text-white bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:border-gray-600 focus:outline-none focus:ring-0 focus:border-[#d34202] peer"
+                      placeholder=" "
+                      required
+                    />
+                    <label
+                      for="floating_phone"
+                      class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 left-2 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-[#d34202] peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
+                      >Phone number (+257 66 666 666)</label
+                    >
+                  </div>
+                  <div class="relative z-0 w-full mb-5 group">
+                    <textarea
+                      type="text"
+                      name="floating_company_info"
+                      id="floating_company_info"
+                      rows="4"
+                      class="block py-2.5 px-0 w-full text-sm text-white bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 focus:outline-none focus:ring-0 focus:border-[#d34202] peer"
+                      placeholder=" "
+                      required
+                    ></textarea>
+                    <label
+                      for="floating_company_info"
+                      class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 left-2 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-[#d34202] peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
+                      >What is your company about ?</label
+                    >
+                  </div>
+                  <div class="relative z-0 w-full mb-5 group">
+                    <textarea
+                      type="text"
+                      name="floating_space_need"
+                      id="floating_space_need"
+                      rows="4"
+                      class="block py-2.5 px-0 w-full text-sm text-white bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:border-gray-600 focus:outline-none focus:ring-0 focus:border-[#d34202] peer"
+                      placeholder=" "
+                      required
+                    ></textarea>
+                    <label
+                      for="floating_space_need"
+                      class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 left-2 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-[#d34202] peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
+                      >How much space will your business need?</label
+                    >
+                  </div>
+                  <button
+                    type="submit"
+                    class="text-white bg-[#ff5722] hover:bg-[#ff3c22e0] focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center"
+                  >
+                    Submit
+                  </button>
+                </form>
+              </div>
             </div>
-            <div class="relative z-0 w-full mb-5 group">
-              <input
-                type="email"
-                name="floating_email"
-                id="floating_email"
-                class="block py-2.5 px-0 w-full text-sm text-white bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 focus:outline-none focus:ring-0 focus:border-[#d34202] peer"
-                placeholder=" "
-                required
-              />
-              <label
-                for="floating_email"
-                class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 left-2 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto peer-focus:text-[#d34202] peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
-                >Email address</label
+            <h2 id="accordion-collapse-heading-2">
+              <button
+                type="button"
+                class="flex items-center justify-between w-full p-5 font-medium rtl:text-right text-gray-500 border border-b-0 border-gray-200 focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-800 dark:border-gray-700 dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-800 gap-3"
+                data-accordion-target="#accordion-collapse-body-2"
+                aria-expanded="false"
+                aria-controls="accordion-collapse-body-2"
               >
-            </div>
-
-            <div class="relative z-0 w-full mb-5 group">
-              <input
-                type="text"
-                name="floating_company"
-                id="floating_company"
-                class="block py-2.5 px-0 w-full text-sm text-white bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 focus:outline-none focus:ring-0 focus:border-[#d34202] peer"
-                placeholder=" "
-                required
-              />
-              <label
-                for="floating_company"
-                class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 left-2 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-[#d34202] peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
-                >Company (Ex. Nuyu Agency)</label
+                <span class="text-white">Register as Sponsor</span>
+                <svg
+                  data-accordion-icon
+                  class="w-3 h-3 rotate-90 shrink-0"
+                  aria-hidden="true"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 10 6"
+                >
+                  <path
+                    stroke="currentColor"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    stroke-width="2"
+                    d="M9 5 5 1 1 5"
+                  />
+                </svg>
+              </button>
+            </h2>
+            <h2 id="accordion-collapse-heading-3">
+              <button
+                type="button"
+                class="flex items-center justify-between w-full p-5 font-medium rtl:text-right text-gray-500 border border-gray-200 focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-800 dark:border-gray-700 dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-800 gap-3"
+                data-accordion-target="#accordion-collapse-body-3"
+                aria-expanded="false"
+                aria-controls="accordion-collapse-body-3"
               >
-            </div>
-            <div class="relative z-0 w-full mb-5 group">
-              <input
-                type="tel"
-                name="floating_phone"
-                id="floating_phone"
-                class="block py-2.5 px-0 w-full text-sm text-white bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:border-gray-600 focus:outline-none focus:ring-0 focus:border-[#d34202] peer"
-                placeholder=" "
-                required
-              />
-              <label
-                for="floating_phone"
-                class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 left-2 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-[#d34202] peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
-                >Phone number (+257 66 666 666)</label
-              >
-            </div>
-            <div class="relative z-0 w-full mb-5 group">
-              <textarea
-                type="text"
-                name="floating_company_info"
-                id="floating_company_info"
-                rows="4"
-                class="block py-2.5 px-0 w-full text-sm text-white bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 focus:outline-none focus:ring-0 focus:border-[#d34202] peer"
-                placeholder=" "
-                required
-              ></textarea>
-              <label
-                for="floating_company_info"
-                class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 left-2 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-[#d34202] peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
-                >What is your company about ?</label
-              >
-            </div>
-            <div class="relative z-0 w-full mb-5 group">
-              <textarea
-                type="text"
-                name="floating_space_need"
-                id="floating_space_need"
-                rows="4"
-                class="block py-2.5 px-0 w-full text-sm text-white bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:border-gray-600 focus:outline-none focus:ring-0 focus:border-[#d34202] peer"
-                placeholder=" "
-                required
-              ></textarea>
-              <label
-                for="floating_space_need"
-                class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 left-2 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-[#d34202] peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
-                >How much space will your business need?</label
-              >
-            </div>
-            <button
-              type="submit"
-              class="text-white bg-[#ff5722] hover:bg-[#ff3c22e0] focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center"
-            >
-              Submit
-            </button>
-          </form>
-
-          <RegistrationBtn message="Register as Sponsor" />
+                <span class="text-white">Register as Exhibitor</span>
+                <svg
+                  data-accordion-icon
+                  class="w-3 h-3 rotate-90 shrink-0"
+                  aria-hidden="true"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 10 6"
+                >
+                  <path
+                    stroke="currentColor"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    stroke-width="2"
+                    d="M9 5 5 1 1 5"
+                  />
+                </svg>
+              </button>
+            </h2>
+          </div>
         </div>
       </div>
     </div>
@@ -149,8 +235,4 @@ const formHidden = ref(true)
   </section>
 </template>
 
-<style scoped>
-h2 span {
-  color: #ff5722;
-}
-</style>
+<style scoped></style>
