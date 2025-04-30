906de4cabdacf7d7535a0761ec67364b0bcedd08
ArtcalO <niyca14@gmail.com>
2025-04-30 14:08:01 +0200

    remove em

--- Backend/tagcon/urls.py
+++ Backend/tagcon/urls.py
@@ -1,6 +1,5 @@
 from django.contrib import admin
 from django.urls import path, include, re_path
-from django.conf.urls.static import static
 from django.views.generic.base import RedirectView
 from . import settings
 
@@ -12,4 +11,4 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/',include('api.urls')),
     path('',RedirectView.as_view(url='/api/')),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
