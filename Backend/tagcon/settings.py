c2406ec5480cf129c4f3f237165f3d30dc6dbfbf
ArtcalO <niyca14@gmail.com>
2025-04-30 10:29:06 +0200

    debug to true

--- Backend/tagcon/settings.py
+++ Backend/tagcon/settings.py
@@ -12,7 +12,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-8d+99e4$4w5vxh+m-5)-i32#utz=4j(0r8tj3+gnd8)h*%^hr5'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
